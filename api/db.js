1be649c63ebe0caec7b5e0344ecae186be126b5e
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-07-08 13:36:08 -0300

    crud simples com nodejs, react e mysql

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
 import mysql from 'mysql';
 
 export const db = mysql.createConnection({
-	host: 'localhost',
-	user: 'henrique',
-	password: 'slipknot1994',
-	database: 'crud',
+	host: 'mysql://root:YOviWunqvBGdJdONUNoKrWlgJnAMcIEN@roundhouse.proxy.rlwy.net:57837/railway',
+	user: 'root',
+	password: 'YOviWunqvBGdJdONUNoKrWlgJnAMcIEN',
+	database: 'mysql',
 });
