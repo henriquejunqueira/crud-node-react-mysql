7aa6fa469081631731846962e4a23b7c2440fce9
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-07-08 15:32:33 -0300

    modificado bd

--- api/db.js
+++ api/db.js
@@ -1,9 +1,9 @@
 import mysql from 'mysql';
 
 export const db = mysql.createConnection({
-	host: process.env.MYSQLHOST,
-	user: process.env.MYSQLUSER,
-	password: process.env.MYSQLPASSWORD,
-	database: process.env.MYSQLDATABASE,
-	port: process.env.MYSQLPORT,
+	host: process.env.DB_HOST,
+	user: process.env.DB_USERNAME,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DBNAME,
+	port: process.env.DB_PORT,
 });
